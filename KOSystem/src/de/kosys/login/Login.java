8e6681bbf0eccd9d34d026e29d905107091305bc
falscherIdiot <falscherIdiot@gmail.com>
2020-05-31 14:43:39 +0200

    Login Frame

---
+++ KOSystem/src/de/kosys/login/Login.java
@@ -0,0 +1,80 @@
+package de.kosys.login;
+
+import java.awt.Color;
+//import java.awt.Graphics;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class Login extends JFrame{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private JPasswordField passwordField;
+	private JTextField usernameField;
+
+	public Login() {
+		getContentPane().setBackground(Color.GRAY);
+		getContentPane().setLayout(null);
+		
+		JPanel top = new JPanel();
+		top.setLocation(0, 0);
+		top.setSize(800,26);
+		getContentPane().add(top);
+		top.setLayout(null);
+		
+		JLabel title = new JLabel("Login");
+		title.setHorizontalAlignment(SwingConstants.CENTER);
+		title.setBounds(349, 5, 102, 14);
+		top.add(title);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(260, 286, 306, 44);
+		getContentPane().add(passwordField);
+		
+		usernameField = new JTextField();
+		usernameField.setBounds(260, 231, 306, 44);
+		getContentPane().add(usernameField);
+		usernameField.setColumns(10);
+		
+		JLabel usernameLabel = new JLabel("Username :");
+		usernameLabel.setFont(new Font("Ink Free", Font.BOLD, 14));
+		usernameLabel.setBounds(148, 231, 102, 44);
+		getContentPane().add(usernameLabel);
+		
+		JLabel passwordLable = new JLabel("Password : ");
+		passwordLable.setFont(new Font("Ink Free", Font.BOLD, 14));
+		passwordLable.setBounds(148, 286, 102, 44);
+		getContentPane().add(passwordLable);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(800,600);
+		setUndecorated(true);
+		setBackground(Color.DARK_GRAY);
+		
+		JButton closebtn = new JButton("X");
+		closebtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		closebtn.setBorder(null);
+		closebtn.setBackground(new Color(0,0,0,0));
+		closebtn.setBounds(726, 0, 74, 26);
+		top.add(closebtn);
+		
+		
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+}
---
+++ KOSystem/src/de/kosys/main/Kosys.java
@@ -0,0 +1,11 @@
+package de.kosys.main;
+
+import de.kosys.login.Login;
+
+public class Kosys {
+
+	public static void main(String[] args) {
+		new Login();		
+	}
+
+}
